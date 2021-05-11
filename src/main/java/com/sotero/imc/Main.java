1cdf7b604942d23fbab91e570040755218ef2f60
André Sotero <andre.sotero@telefonica.com>
2021-05-11 00:09:02 -0300

    Alteracao no cabecalho e rodape

--- src/main/java/com/sotero/imc/Main.java
+++ src/main/java/com/sotero/imc/Main.java
@@ -11,27 +11,23 @@ import java.util.Scanner;
  *
  * @author usuario
  */
-
-
 public class Main {
-    
-    private static Scanner scanf = new Scanner (System.in);
-    
+
+    private static Scanner scanf = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
         /*
         FAZER UM PROGRAMA QUE LÊ NOME, SOBRENOME, ENDEREÇO. E no final, mostrar o resultado.
-        */
-        
-        System.out.println("Programa Imc");
-        
+         */
+        System.out.println(":::...CADASTRO DE USUARIO...:::");
+
         System.out.println("Informe o seu nome moucher");
-        
+
         String nome = scanf.nextLine();
-       
+
         System.out.println("O nome informado foi: " + nome);
-        
-        System.out.println("FIM DO PROGRAMA IMC");
-        
+
+        System.out.println(":::...FIM CADASTRO DE USUARIO...:::");
     }
 }
