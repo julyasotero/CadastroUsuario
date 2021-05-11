6bdd6dccb02ab141f460c7a8f6e7d310a9e885d5
julyasotero <julyasotero@gmail.com>
2021-05-11 11:03:58 -0300

    atividade 01

--- src/main/java/com/sotero/imc/Main.java
+++ src/main/java/com/sotero/imc/Main.java
@@ -20,7 +20,7 @@ public class Main {
         /*
         FAZER UM PROGRAMA QUE LÊ NOME, SOBRENOME, ENDEREÇO. E no final, mostrar o resultado.
          */
-        System.out.println(":::...CADASTRO DE USUARIO...:::");
+ /*System.out.println(":::...CADASTRO DE USUARIO...:::");
 
         System.out.println("Informe o seu nome moucher");
 
@@ -29,5 +29,26 @@ public class Main {
         System.out.println("O nome informado foi: " + nome);
 
         System.out.println(":::...FIM CADASTRO DE USUARIO...:::");
+         */
+        System.out.println("CADASTRO USUARIO");
+
+        System.out.println("Infome seu Nome");
+
+        String nome = scanf.nextLine();
+
+        System.out.println("Informe seu SobreNome");
+
+        String sobreNome = scanf.nextLine();
+
+        System.out.println("Informe seu Endereco");
+
+        String endereco = scanf.nextLine();
+
+        System.out.println("Final do cadastro");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("SobreNome: " + sobreNome);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("Final do cadastro");
     }
 }
